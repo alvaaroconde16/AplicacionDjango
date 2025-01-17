a8a642db191b2a0bf43b2f65deb51b137dfa9944
Alvaro Conde <alvaro.conde.soria.al@iespoligonosur.org>
2025-01-17 13:43:13 +0100

    Aplicacion Django

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,17 +8,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-#add yout project directory to the sys.path
-project_home = 'home/alvaroconde/AplicacionDjango/mysite'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
-
-
-#set environment variable to tell django where your settings.py is 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
 
 #serve django via WSGI
 from django.core.wsgi import get_wsgi_application
