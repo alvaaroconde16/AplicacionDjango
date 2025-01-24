4a445d38679076957c165d0e04f3a19d8181fda0
Alvaro Conde <alvaro.conde.soria.al@iespoligonosur.org>
2025-01-24 11:59:37 +0100

    Aplicacion Django

--- mysite/settings.py
+++ mysite/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'django_bootstrap5',
     'django_bootstrap_icons',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ viajes/api_views.py
@@ -0,0 +1,11 @@
+from .models import *
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .forms import *
+
+
+@api_view(['GET'])
+def reserva_list(request):
+    
+    reservas = Reserva.objects.all()
